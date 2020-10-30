e464501ea42ea8c3ccd932c4894575a763054674
danielxu <danielxu@localhost>
2020-10-29 19:51:00 -0700

    Updated WordRecommender

--- Spellcheck/src/WordRecommender.java
+++ Spellcheck/src/WordRecommender.java
@@ -9,13 +9,13 @@ public class WordRecommender {
 	String filename; //refers to the file 
 	//containing a dictionary of correct words
 	//ensure that spellcheck can run with any file name
-	
-	
+
+
 	String word1;
 	String word2;
-	ArrayList<String> WordsFromDictionary;
+	ArrayList<String> wordsFromDictionary;
 	ArrayList<String> wordsFromUserFile;
-	
+
 	//Constructor for taking dictionary File
 	public WordRecommender (String fileName, String checkFile) {
 		//create objective
@@ -23,51 +23,44 @@ public class WordRecommender {
 		String userCheckFile = checkFile;
 		//set as new
 		File userFile = new File(userDictionary);
-		
+
 		try {
 			//create ArrayList wordsFromUserFile
 			Scanner fileScanner = new Scanner(userFile);
 			while (fileScanner.hasNextLine()) { //This would need to be changed
-				wordsFromUserFile.add(fileScanner.nextLine());
+				wordsFromDictionary.add(fileScanner.nextLine());
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("Invalid file");
 			e.printStackTrace();
-			
+
 		}
-		
+
 		/**
 		 * Once ArrayList<String> for userDictionary is created, now create 
 		 * a ArrayList for all words to check spelling
 		 */
-	
-		//need method to change entire file to stringString
-		String listOfPpl = "arv, aash, dawn";
-		
-		//this allows you to read csv files 
-		String[] wordsFromUserFile = listOfPpl.split(" "); //use Array or ArrayList?
-		System.out.println(Arrays.toString(wordsFromUserFile));
-		
-		
+		//System.out.println(Arrays.toString(wordsFromUserFile))
+
 		//created a handle/file object
 		File f = new File(userCheckFile);
 		try {
 			//try to do stuff
 			Scanner fileScanner = new Scanner(f);
 			//need to ask about this
-			while (fileScanner.hasNextLine()) {
-				System.out.println(fileScanner.nextLine());
+			while (fileScanner.hasNext()) {
+				wordsFromUserFile.add(fileScanner.next());
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("bad file name");
 			e.printStackTrace();
 		}
 	}
-		
-		
-	}
 
-	
+
+}
+
+
 
 //	String str = "2346512aa";
 //	int sum = 0;
@@ -89,10 +82,10 @@ public class WordRecommender {
 //	System.out.println("Sum: " + sum);
 //} //main
 //class
-		
-	
-		
-		
+
+
+
+
 //	//get similarities and return average of the two words
 //	//maybe use 2 forloops
 //	public double getSimilarity (String word1, String word2) {
@@ -110,8 +103,8 @@ public class WordRecommender {
 //		
 //	}
 //	}
-	
-	
+
+
 
 
 
