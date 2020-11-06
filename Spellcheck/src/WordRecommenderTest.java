37dde3e6c7db540a781ea04722ff694f79f7f6f9
coreyparker <coreyparker@coreys-mbp>
2020-11-05 20:19:10 -0500

    unit tested

--- Spellcheck/src/WordRecommenderTest.java
+++ Spellcheck/src/WordRecommenderTest.java
@@ -15,66 +15,86 @@ class WordRecommenderTest{
 	ArrayList<String> wordsFromUserFile;
 
 	@Test
-	void testCreation() {
+	void testInstance() {
+		WordRecommender a = new WordRecommender();
+		assertNotNull(a);
+	}
+	
+	@Test
+	void testFileCreation() {
 		WordRecommender a = new WordRecommender();
 		assertNotNull(a);
 		wordsFromDictionary = a.createDFile("testdictionary");
 		assertNotNull(wordsFromDictionary);
 	    wordsFromUserFile = a.createFileToCheck("usertocorrect.txt");
 	}
-//	@Test
-//	void setUp(String fileName) throws Exception {
-//		WordRecommender a = new WordRecommender();
-//		assertNotNull(a);
-//	    wordsFromDictionary = a.createDFile("testdictionary");
-//	    wordsFromUserFile = a.createFileToCheck("usertocorrect.txt");
-//	 	
-//	}
 
 	@Test
 	void testFileDCreation() {
-	    assertEquals("hello", wordsFromDictionary.get(0), "First word correct");
+		WordRecommender a = new WordRecommender();
+		wordsFromDictionary = a.createDFile("testdictionary");
+		assertEquals("hello", wordsFromDictionary.get(0), "First word in dictionary");
 		
 	}
 	
 	@Test
 	void testFileCCreation() {
-		assertEquals("hellp", wordsFromUserFile.get(0), "First word correct");
-	}
-	
-	
-	
-	
-	
-	
-	
-	@Test
-	void createCorrectDictionaryFile() {
-		
-		fail("Not yet implemented");
+		WordRecommender a = new WordRecommender();
+		wordsFromUserFile = a.createFileToCheck("usertocorrect.txt");
+		assertEquals("hellp", wordsFromUserFile.get(0), "First word in file");
 	}
 	
 	@Test
-	void createCorrectWordsToCheckFile() {
-		
-		fail("Not yet implemented");
+	void getSim1() {
+		WordRecommender a = new WordRecommender();
+		double Sim = a.getSimilarity("help", "hell");
+		assertEquals(3, Sim, "First word in file");
+	}
+	
+	@Test
+	void getSim2() {
+		WordRecommender a = new WordRecommender();
+		double Sim = a.getSimilarity("help", "hello");
+		assertEquals(Sim, 2, "Sim works");
+	}
+	
+
+	@Test
+	void print() {
+		WordRecommender a = new WordRecommender();
+		ArrayList<String> list =  new ArrayList <String>();
+		list.add("hi");
+		list.add("hello");
+		list.add("help");
+		String toPrint = a.prettyPrint(list);
+		assertNotNull(toPrint);
+	}
+	
+	@Test
+	void getWordS1() {
+		WordRecommender a = new WordRecommender();
+		a.createFileToCheck("usertocorrect.txt");
+		a.createDFile("testdictionary");
+		ArrayList<String> words = a.getWordSuggestions("hellp", 2, 0.8, 5);
+		assertNotNull(words);
+	}
+	
+	@Test
+	void getWordS2() {
+		WordRecommender a = new WordRecommender();
+		a.createFileToCheck("usertocorrect.txt");
+		a.createDFile("testdictionary");
+		ArrayList<String> words = a.getWordSuggestions("hellp", 1, 0.8, 2);
+		assertNotNull(words);
 	}
 	
 	
 	
-//	WordRecommender a = new WordRecommender();
-//	static ArrayList <String> newFileD = a.createDFile("engDictionary.txt");
-//	ArrayList <String> newFileC = a.createFileToCheck("usertocorrect");
-//	
-//	
-//	public static void main(String[] args) {
-//		
-//		
-//		for (int i = 0; i < 5; i++) {
-//			System.out.println(newFileD.get(i));
-//		}
-//	
-//	}
+	
+	
+	
+	
+
 	
 
 }
